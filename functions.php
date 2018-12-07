13b60153877fceb237d0bf22b80d6713301e1089
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-12-07 21:25:42 +0800

    load js menggunakan functions

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 function load_theme(){
   wp_enqueue_style( 'style', get_template_directory_uri().'/css/main.css');
+  wp_enqueue_script( 'app', get_template_directory_uri().'/js/app.js');
 }
 add_action( 'wp_enqueue_scripts', 'load_theme' );
--- header.php
+++ header.php
@@ -5,6 +5,5 @@
     <title>SUAR SUARA</title>
     <?php wp_head() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="<?= get_template_directory_uri()."/js/app.js" ?>"></script>
   </head>
   <body>
