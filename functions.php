8e154a1f5e2203925288d729b5caa0d05054b751
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-12-10 14:47:57 +0800

    fungsi post slider

--- functions.php
+++ functions.php
@@ -38,3 +38,24 @@ if (is_admin()) {
     }
   });
 }
+
+function slider_post_init() {
+  $args = array(
+    'label' => 'Slider',
+    'public' => true,
+    'show_ui' => true,
+    'capability_type' => 'post',
+    'hierarchical' => false,
+    'rewrite' => array('slug' => 'slider'),
+    'query_var' => true,
+    'menu_icon' => 'dashicons-slides',
+    'supports' => array(
+      'title',
+      'thumbnail',
+      'author',
+      'page-attributes',
+    )
+  );
+  register_post_type( 'slider', $args );
+}
+add_action( 'init', 'slider_post_init' );
--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 <div class="uk-container">
   <div class="uk-position-relative uk-visible-toggle uk-margin-medium-top" uk-slideshow="autoplay:true; ratio: 5:2; animation: fade">
     <ul class="uk-slideshow-items">
-      <li><img src="<?= get_template_directory_uri()."/img/slides/slide1.jpg" ?>" alt="image" uk-cover></li>
-      <li><img src="<?= get_template_directory_uri()."/img/slides/slide2.jpg" ?>" alt="image" uk-cover></li>
-      <li><img src="<?= get_template_directory_uri()."/img/slides/slide3.jpg" ?>" alt="image" uk-cover></li>
+      <?php
+        $query = new WP_Query( array('post_type' => 'slider') );
+        while ( $query->have_posts() ) : $query->the_post();
+      ?>
+        <li><img src="<?= wp_get_attachment_url(get_post_thumbnail_id()) ?>" alt="image" uk-cover></li>
+      <?php endwhile ?>
     </ul>
     <a class="uk-slidenav-large uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
     <a class="uk-slidenav-large uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
     <ul class="uk-margin-remove uk-slideshow-nav uk-dotnav uk-flex-center uk-margin"></ul>
   </div>
-
   <section id="content">
     <div uk-grid>
       <div class="uk-width-2-3@m">
@@ -83,7 +85,6 @@
       </div>
       <div class="uk-width-1-3@m">
         <h1 class="main-title">HIGHLIGHT VIDEO</h1>
-
         <div class="videowrapper">
           <iframe width="auto" height="100%" allowfullscreen
           src="<?= get_option('url_highlight_youtube') ?>" class="video">
@@ -108,7 +109,6 @@
               <li>
                 <span uk-icon="icon:calendar; ratio:.8"></span>
                 <span class="uk-text-middle uk-margin-small-right">10 Oktober 2018</span>
-
                 <span uk-icon="icon:clock; ratio:.8"></span>
                 <span class="uk-text-middle">15:00 - Finish</span>
               </li>
@@ -124,7 +124,6 @@
               <li>
                 <span uk-icon="icon:calendar; ratio:.8"></span>
                 <span class="uk-text-middle uk-margin-small-right">10 Oktober 2018</span>
-
                 <span uk-icon="icon:clock; ratio:.8"></span>
                 <span class="uk-text-middle">15:00 - Finish</span>
               </li>
@@ -140,7 +139,6 @@
               <li>
                 <span uk-icon="icon:calendar; ratio:.8"></span>
                 <span class="uk-text-middle uk-margin-small-right">10 Oktober 2018</span>
-
                 <span uk-icon="icon:clock; ratio:.8"></span>
                 <span class="uk-text-middle">15:00 - Finish</span>
               </li>
@@ -156,7 +154,6 @@
               <li>
                 <span uk-icon="icon:calendar; ratio:.8"></span>
                 <span class="uk-text-middle uk-margin-small-right">10 Oktober 2018</span>
-
                 <span uk-icon="icon:clock; ratio:.8"></span>
                 <span class="uk-text-middle">15:00 - Finish</span>
               </li>
@@ -166,14 +163,10 @@
               </li>
             </ul>
           </div>
-
         </section>
       </div>
     </div>
   </div>
 </section>
-<section id="info">
-</section>
-
 </div>
 <?php get_footer() ?>
