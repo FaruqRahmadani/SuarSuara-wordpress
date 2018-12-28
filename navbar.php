9167d697b78e82ac72e2268cfc3c25977d3ada13
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-12-28 14:32:56 +0800

    perbaikan navbar dan menambah page berita

--- navbar.php
+++ navbar.php
@@ -8,9 +8,10 @@
         <div class="main-menu">
           <ul class="uk-navbar-nav">
             <li><a href="<?= get_home_url() ?>">BERANDA</a></li>
-            <li><a href="#">BERITA</a></li>
             <li><a href="<?= get_category_link(get_cat_ID('Band')) ?>">STORE</a></li>
-            <li><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Tentang Kami' ) ) ); ?>">TENTANG KAMI</a></li>
+            <?php wp_list_pages( array(
+              'title_li'    => '',
+            ) ) ?>
             <li><a class="uk-navbar-toggle uk-float-right uk-padding-small" href="#search" uk-search-icon uk-toggle></a></li>
           </ul>
         </div>
