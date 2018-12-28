f9f302c96ac54ac99991e424318539dd7f1b4de5
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-12-28 15:28:17 +0800

    undi

--- functions.php
+++ functions.php
@@ -82,11 +82,3 @@ function set_excerpt_text(){
 
 add_filter('excerpt_length', 'set_excerpt_length');
 add_filter('excerpt_more', 'set_excerpt_text');
-
-function hs_image_editor_default_to_gd( $editors ) {
-  $gd_editor = 'WP_Image_Editor_GD';
-  $editors = array_diff( $editors, array( $gd_editor ) );
-  array_unshift( $editors, $gd_editor );
-  return $editors;
-}
-add_filter( 'wp_image_editors', 'hs_image_editor_default_to_gd' );
