70fa8baa5d3b5b4cbe1190d2bf5663f580d7f749
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-12-29 11:30:28 +0800

    perbaikan category band

--- category-band.php
+++ category-band.php
@@ -2,11 +2,11 @@
 <div class="uk-container">
   <section id="content">
     <h1 class="main-title">PRODUCT</h1>
-    <?php
+    <div class="uk-grid">
+      <?php
       $query = new WP_Query( array('post_type' => 'bands') );
       while ( $query->have_posts() ) : $query->the_post();
-    ?>
-    <div class="uk-grid">
+      ?>
       <div class="uk-width-1 uk-width-1-2@s uk-width-1-3@m uk-width-1-4@l">
         <div class="frame">
           <a href="<?= get_the_permalink() ?>"><img src="<?= wp_get_attachment_url(get_post_thumbnail_id()) ?>" alt="images">
@@ -14,10 +14,10 @@
           </a>
         </div>
       </div>
-    </div>
-    <?php
+      <?php
       endwhile;
-    ?>
+      ?>
+    </div>
   </section>
 </div>
 <?php get_footer() ?>
