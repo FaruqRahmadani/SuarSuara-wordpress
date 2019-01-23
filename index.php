921e7d874a8ed68766d71c5e40f992cbeed3f9a5
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-01-23 17:15:45 +0700

    perbaiki slideshow dengan class uikit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 <div class="uk-container">
-  <div class="uk-position-relative uk-visible-toggle uk-margin-medium-top" uk-slideshow="autoplay:true; ratio: 5:2; animation: fade">
-    <ul class="uk-slideshow-items custom-slideshow">
+  <div class="uk-position-relative uk-visible-toggle uk-margin-medium-top" uk-slideshow="min-height: 600; autoplay:true; ratio: 5:2; animation: fade">
+    <ul class="uk-slideshow-items">
       <?php
         $query = new WP_Query( array('post_type' => 'slider') );
         while ( $query->have_posts() ) : $query->the_post();
